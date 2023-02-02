cb90e09c73f80751ae4a6ac5f64b8cce4f28977e
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-02 17:07:09 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd /home/ubuntu/server
+cd /home/ubuntu/
 
 #_Update_&_Set_Node_Version
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
@@ -13,3 +13,12 @@ nvm install 16
 
 #_Download_PM2
 npm install pm2@latest -g
+
+#create our working directory if it doesnt exist
+DIR="/home/ubuntu/server"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
