447adb8459fb9af6ad73f3f0613ca8a1019845c2
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-03 10:04:19 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-#_Change_Working_Directory
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ubuntu/server
+
+#navigate into our working directory where we have all our github files
 cd /home/ubuntu/server
 
-#_Remove_Unused_Code
-rm -rf node_modules
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#Install_node_modules_&_Make_React_Build
-npm  install
+#install node modules
+npm install
+
+#start our node app in the background
+node app.js > app.out.log 2> app.err.log < /dev/null & 
